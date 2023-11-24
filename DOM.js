c0a4b151b5a0f7e2252bb3c9eb6c814969b64078
LAPTOP-M8V9U3AJ\davic <davicho.DL1@gmail.com>
2023-11-24 00:37:26 -0500

    Lascano

--- DOM.js
+++ DOM.js
@@ -12,9 +12,22 @@
 
 /* Agregar elementos */
 
-
+//-----------------------------------------------------------------------------------
 /* Eliminar elementos */
+// Eliminar tareas pendientes ya realizadas
 
+// Obtener el elemento que se desea eliminar
+var elementoEliminar = document.getElementById("miTarea");
+
+// Verificar si el elemento existe antes de intentar eliminarlo
+if (elementoEliminar) {
+  // Obtener el padre del elemento
+  var padreElemento = elementoEliminar.parentNode;
+
+  // Eliminar el elemento del DOM
+  padreElemento.removeChild(elementoEliminar);
+}
+//----------------------------------------------------------------------------------------
 
 /* Eventos */
 
